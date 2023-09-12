66c573886eeb3ad8ea9571fa9cf706f5b3e54426
balukittu50 <balukittu50@gmail.com>
2023-09-12 16:51:18 +0530

    first commit

---
+++ src/javanotes/InsatanceVaraibaleEmp.java
@@ -0,0 +1,30 @@
+package javanotes;
+
+public class InsatanceVaraibaleEmp {
+	public String name;
+	private double salary;
+
+	public InsatanceVaraibaleEmp(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void Emp(String empname) {
+
+		name = empname;
+	}
+
+	public void setSalary(double Empsal) {
+		salary = Empsal;
+	}
+
+	public void PrintEmp() {
+		System.out.println("name" + name);
+		System.out.println("salary" + salary);
+
+	}
+	public void main(String args[]) {
+		InsatanceVaraibaleEmp one=new InsatanceVaraibaleEmp("kittu");
+		one.setSalary(1000);
+		one.PrintEmp();
+	}
+}
